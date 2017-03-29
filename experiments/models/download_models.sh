0ea7511a6f64710c605dca9c5ae6648860b8b4d8
zhanghang1989 <zhang.hang@rutgers.edu>
2017-03-28 22:01:43 -0400

    url

--- experiments/models/download_models.sh
+++ experiments/models/download_models.sh
@@ -1,4 +1,4 @@
 cd models
-curl -O "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7"
-curl -O "http://hangzh.com/models/model_9styles.t7"
+wget -O vgg16.t7 "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7"
+wget -O model_9styles.t7 "https://www.dropbox.com/s/50bn53g0ok26aac/model_9styles.t7?dl=1"
 cd ../
